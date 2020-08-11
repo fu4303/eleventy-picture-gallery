000e0e9e0e18ff40d4fc2d823d84c737df61b85e
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-08-11 15:50:15 +0200

    👌 IMPROVE: Remove rss-plugin, use in-built url filter, fix meta.json

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,7 @@
-const pluginRss = require('@11ty/eleventy-plugin-rss')
 const cssMinFilter = require('./src/filters/css-min-filter.js');
 const htmlMinTransform = require('./src/transforms/html-min-transform.js');
 
 module.exports = (config) => {
-  config.addPlugin(pluginRss)
-
 
   const isProduction = process.env.NODE_ENV === 'production'
   if (isProduction) {
